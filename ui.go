a0c6755561e8a8ad28786ddb94b752cba4e7434b
George Ribeiro <george@sobral.ufc.br>
2022-02-09 16:46:41 -0300

    interface quase terminada

--- ui.go
+++ ui.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/rivo/tview"
 )
@@ -15,12 +17,44 @@ type BlockChainUI struct {
 
 func NewBlockChainUI(ps *pubsub.PubSub) *BlockChainUI {
 	app := tview.NewApplication()
-	box := tview.NewBox().SetBorder(true).SetTitle("Blockchain - UFCoin")
-	app.SetRoot(box, true)
+
+	doneCh := make(chan struct{}, 1)
+
+	logBox := tview.NewTextView()
+	logBox.SetDynamicColors(true)
+	logBox.SetBorder(true)
+	logBox.SetTitle(fmt.Sprintf("Log: %s", "1"))
+	logBox.SetChangedFunc(func() {
+		app.Draw()
+	})
+
+	peersList := tview.NewTextView()
+	peersList.SetBorder(true)
+	peersList.Box.SetTitle("Conectados")
+	peersList.SetChangedFunc(func() {
+		app.Draw()
+	})
+
+	mnList := tview.NewList().
+		AddItem("Transferir", "Transferência de valores entre carteiras", 't', nil).
+		AddItem("Sair", "Sair do Sistema", 's', func() {
+			app.Stop()
+			doneCh <- struct{}{}
+		})
+	mnList.SetBorder(true)
+	mnList.SetTitle("Menu")
+
+	panel := tview.NewFlex().
+		AddItem(mnList, 20, 1, true).
+		AddItem(logBox, 0, 1, false).
+		AddItem(peersList, 20, 1, false)
+
+	app.SetRoot(panel, true).EnableMouse(true)
 
 	return &BlockChainUI{
-		app: app,
-		ps:  ps,
+		app:    app,
+		ps:     ps,
+		doneCh: doneCh,
 	}
 }
 
@@ -41,4 +75,5 @@ func (ui *BlockChainUI) handleEvents() {
 
 func (ui *BlockChainUI) End() {
 	ui.doneCh <- struct{}{}
+	ui.app.Stop()
 }
